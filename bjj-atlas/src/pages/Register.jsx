8789f5ef731e931e3b34cb9023f6606894f8e50d
Vagner-Sander <vagnerarthurss09@gmail.com>
2025-11-12 01:17:11 -0300

    progredindo desenvolvimento

--- bjj-atlas/src/pages/Register.jsx
+++ bjj-atlas/src/pages/Register.jsx
@@ -10,24 +10,64 @@ export default function Register() {
   function handleSubmit(e) {
     e.preventDefault()
     localStorage.setItem('user', JSON.stringify(form))
-    alert('Cadastro feito!')
+    alert('Cadastro feito com sucesso!')
+    setForm({ nome: '', email: '', idade: '', faixa: 'branca' }) // limpa o formulário
   }
 
   return (
-    <div style={{ padding: 30 }}>
+    <div className='flex items-center justify-start flex-col pt-20 min-h-screen'>
+      <div></div>
       <h2>Cadastro</h2>
-      <form onSubmit={handleSubmit}>
-        <input placeholder="Nome" name="nome" onChange={handleChange} />
-        <input placeholder="Email" name="email" onChange={handleChange} />
-        <input placeholder="Idade" name="idade" onChange={handleChange} />
-        <select name="faixa" onChange={handleChange}>
-          <option>branca</option>
-          <option>azul</option>
-          <option>roxa</option>
-          <option>marrom</option>
-          <option>preta</option>
+      <form className='flex flex-col w-70' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="nome"
+          placeholder="Nome"
+          value={form.nome}
+          onChange={handleChange}
+          style={{ padding: 10, marginBottom: 10, borderRadius: '0.5rem', border: '1px solid black' }}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={handleChange}
+          style={{ padding: 10, marginBottom: 10, borderRadius: '0.5rem', border: '1px solid black' }}
+        />
+        <input
+          type="number"
+          name="idade"
+          placeholder="Idade"
+          value={form.idade}
+          onChange={handleChange}
+          style={{ padding: 10, marginBottom: 10, borderRadius: '0.5rem', border: '1px solid black' }}
+        />
+        <select
+          name="faixa"
+          value={form.faixa}
+          onChange={handleChange}
+          style={{ padding: 10, marginBottom: 10, borderRadius: '0.5rem', border: '1px solid black' }}
+        >
+          <option value="branca">branca</option>
+          <option value="azul">azul</option>
+          <option value="roxa">roxa</option>
+          <option value="marrom">marrom</option>
+          <option value="preta">preta</option>
         </select>
-        <button type="submit">Cadastrar</button>
+        <button
+          type="submit"
+          style={{
+            padding: 10,
+            marginBottom: 10,
+            borderRadius: '0.5rem',
+            border: '1px solid black',
+            background: '#d11e1eff',
+            color: 'white'
+          }}
+        >
+          Cadastrar
+        </button>
       </form>
     </div>
   )
